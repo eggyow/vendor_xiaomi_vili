93c031270792a70ef876d8acb8d5768807268fbc
AdarshGrewal <adarshgrewal@gmail.com>
2024-09-13 17:04:00 +0530

    vili: Decommonize citsensorservice stack
    Change-Id: I04accb960b46dfafa14c0d6082191912d4d3c64e

--- vili-vendor.mk
+++ vili-vendor.mk
@@ -118,6 +118,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/vili/proprietary/vendor/etc/init/ipa_fws.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/ipa_fws.rc \
     vendor/xiaomi/vili/proprietary/vendor/etc/init/vendor.sensors.qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.sensors.qti.rc \
     vendor/xiaomi/vili/proprietary/vendor/etc/init/vendor.sensors.sscrpcd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.sensors.sscrpcd.rc \
+    vendor/xiaomi/vili/proprietary/vendor/etc/init/vendor.xiaomi.hardware.citsensorservice@1.1-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.xiaomi.hardware.citsensorservice@1.1-service.rc \
     vendor/xiaomi/vili/proprietary/vendor/etc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     vendor/xiaomi/vili/proprietary/vendor/etc/libnfc-nxp-pnscr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp-pnscr.conf \
     vendor/xiaomi/vili/proprietary/vendor/etc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
@@ -569,6 +570,7 @@ PRODUCT_PACKAGES += \
     com.qti.chi.override \
     fingerprint.fpc.default \
     fingerprint.goodix.default \
+    vendor.xiaomi.hardware.citsensorservice@1.1-impl \
     libAF \
     libFlickerDet \
     libI420colorconvert \
@@ -701,6 +703,8 @@ PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.cameraperf@1.0 \
     vendor.xiaomi.hardware.campostproc@1.0-service-impl \
     vendor.xiaomi.hardware.campostproc@1.0 \
+    vendor.xiaomi.hardware.citsensorservice@1.0 \
+    vendor.xiaomi.hardware.citsensorservice@1.1 \
     capi_v2_aptX_CLHDADV_Encoder \
     capi_v2_aptX_CLHDAD_Speech_Decoder \
     capi_v2_cirrus_sp \
@@ -745,7 +749,9 @@ PRODUCT_PACKAGES += \
     misound_karaokemix_res \
     misound_res_headphone \
     misound_res_spk \
+    vendor.xiaomi.hardware.citsensorservice@1.1-service.xml \
     batterysecret \
+    vendor.xiaomi.hardware.citsensorservice@1.1-service \
     init.qcom.sensors \
     sensors.qti \
     sscrpcd
